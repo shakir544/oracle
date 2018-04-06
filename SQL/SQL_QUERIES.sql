11e96a56bea60741e549ca2ddc7d8a6eac52df55
Shakir Gooty <shakir544@gmail.com>
2018-04-06 11:54:36 -0400

    Check audit table space size

--- SQL/SQL_QUERIES.sql
+++ SQL/SQL_QUERIES.sql
@@ -12,6 +12,10 @@ create table new_table as ( select * from old_table);
 INSERT INTO new_table (select * from old_table);
 
 # Copy specifiy columns from old table to new table
-
 INSERT INTO table new_table(col datatype, col2 datatype)
 select col, col2 from old_table;
+
+
+# How to check the audit table space in Oracle
+SELECT segment_name, round(SUM(bytes)/(1024*1024*1024),2) "GB SPACE" from dba_segments
+where segment_name in ('AUD$') group by segment_name order by 2 desc; 
