e591f17aa3ebffea3c3a6d0036a70c205b625359
Shakir Gooty <shakir544@gmail.com>
2018-04-20 22:40:31 -0400

    Update SQL_QUERIES.sql

--- SQL/SQL_QUERIES.sql
+++ SQL/SQL_QUERIES.sql
@@ -5,17 +5,17 @@ rem
 rem  ****************************************************************
 rem
 
-/* Oracle copy data from table to another
+-- Oracle copy data from table to another
 create table new_table as ( select * from old_table);
 
-# If the table schema is already created
+-- If the table schema is already created
 INSERT INTO new_table (select * from old_table);
 
-# Copy specifiy columns from old table to new table
+-- Copy specifiy columns from old table to new table
 INSERT INTO table new_table(col datatype, col2 datatype)
 select col, col2 from old_table;
 
 
-# How to check the audit table space in Oracle
+-- How to check the audit table space in Oracle
 SELECT segment_name, round(SUM(bytes)/(1024*1024*1024),2) "GB SPACE" from dba_segments
 where segment_name in ('AUD$') group by segment_name order by 2 desc; 
